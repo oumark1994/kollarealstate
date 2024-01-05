635d56ffaf57816458a6a9496c24f01dc2fddfc7
@oumar1994 <oumarfilston@gmail.com>
2024-01-04 17:46:24 -0800

    add config

--- api/index.js
+++ api/index.js
@@ -5,7 +5,8 @@ import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
 import listingRouter from "./routes/listing.route.js"
 import cookieParser from "cookie-parser"
-import cors from "cors"
+import path from 'path'
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 
@@ -20,6 +21,10 @@ mongoose.connect(process.env.mongourl, { useNewUrlParser: true, useUnifiedTopolo
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing',listingRouter);
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})  
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
